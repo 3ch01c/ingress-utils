92d972ee9b51dd08a41325ec97cebc19f55a6ca6
Jack Miner <3ch01c@gmail.com>
2018-02-25 09:07:01 +0000

    Changed cut to awk for robustness

--- countstats.sh
+++ countstats.sh
@@ -1,6 +1,6 @@
 # Counts player stats and outputs to CSV
 echo "Agent,MU captured,Portals captured,Resonators deployed,Links created,Fields created,Resonators destroyed,Links destroyed,Fields destroyed,MU freed"
-PLAYERS=`cut -f2 $1 | sort -f | uniq | grep '^<' | sed 's/[<>]//g'`
+PLAYERS=`awk '{print $2}' $1 | sort -f | uniq | grep '^<' | sed 's/[<>]//g'`
 for PLAYER in $PLAYERS
 do
   MU=`grep -oE "$PLAYER>\s+.+ \+[0-9]+ MUs" $1 | awk '{s+=$(NF-1)} END {print s}'`
